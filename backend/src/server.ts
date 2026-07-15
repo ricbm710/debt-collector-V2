d2b3c91eead83fe28df960e5b9d4cb9cd2f78263
Ricardo <ricardobarmont@gmail.com>
2026-07-14 21:41:23 -0400

    Initialize React frontend and Express backend

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.send("Backend is running!");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
